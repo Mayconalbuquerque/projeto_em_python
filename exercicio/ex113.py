114814492f9eb00c23c6280c98c3c87ab51c5ccf
Mayconalbuquerque <142945798+Mayconalbuquerque@users.noreply.github.com>
2023-11-08 16:39:07 -0300

    git commit -m

---
+++ exercicio/ex113.py
@@ -0,0 +1,11 @@
+# Exercícios com funçõe
+
+# Crie uma função que multiplica todos os argumentos
+# nao nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne seo número é par ou impar.
+
+
--- intermediario_python/aula112.py
+++ intermediario_python/aula112.py
@@ -1,8 +1,19 @@
 def soma(*args):
     total = 0
-    
-    for numeros in args:
-        total += numeros
-        return total
+    for numero in args:
+        total += numero
+    return total
 
-soma1__2 = soma(1, 2, 3, 4, 5, 6)
+#
+#soma_1_2_3 = soma(1, 2, 3)
+#print(soma_1_2_3)
+#
+#soma_4_5_6 = soma(4, 5, 6)
+#print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
+print(*numeros)
