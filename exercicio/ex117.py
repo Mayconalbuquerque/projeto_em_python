80f206584a8971b70d97209b9df762887188203a
Mayconalbuquerque <142945798+Mayconalbuquerque@users.noreply.github.com>
2023-11-13 16:05:23 -0300

    git commit -m

--- exercicio/ex117.py
+++ exercicio/ex117.py
@@ -4,18 +4,17 @@ Crie funções que duplicam, triplicam e quadriplicam
 o número recebido como parâmetro
 """
 
+number = 4
 
 def duplicate(number):
     return number * 2
 
-def triplicate(number):
-    return number * 3
+    def triplicate(number):
+        return number * 3
 
-def quadruplicate(number):
-    return number * 4
+        def quadruplicate(number):
+            return number * 4
 
-
-print('O dobro no número selecionado é', duplicate(4))
-print('O triplo no número selecionado é', triplicate(4))
-print('O quadruplo no número selecionado é', quadruplicate(4))
-
+print('O dobro no número selecionado é', duplicate(number))
+print('O triplo no número selecionado é', triplicate(number))
+print('O quadruplo no número selecionado é', quadruplicate(number))
