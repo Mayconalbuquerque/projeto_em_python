b7c2b00c64a706915008d6a1c33631a8cb3abfbb
Mayconalbuquerque <mayconalbuquerque10@gmail.com>
2023-11-13 20:41:51 -0300

    git commit -m

--- intermediario_python/aula120.py
+++ intermediario_python/aula120.py
@@ -1,3 +1,15 @@
 # Manipulando chaves e valores em dicionários
+import os
 
-pessoa
+pessoa = {}
+
+
+pessoa['nome'] = 'Maycon'
+pessoa['sobrenome'] = 'Albuquerque'
+
+
+os.system('cls')
+
+print(pessoa)
+print()
+print(pessoa['sobrenome'])
