eb82909d13c9f955c034b027eb44f4d4fa9c71a4
Mayconalbuquerque <142945798+Mayconalbuquerque@users.noreply.github.com>
2023-11-13 15:15:29 -0300

    git commit -m

--- intermediario_python/aula116.py
+++ intermediario_python/aula116.py
@@ -10,14 +10,20 @@ print(greeting_1)
 print(greeting_2)
 """
 
+lista = ['Maycon', 'Vitória', 'Fátima', 'Rodrigo']
 
-def create_greeting(greeting, name):
-    def greet():
+def create_greeting(greeting):
+    def greet(name):
         return f'{greeting}, {name}'
-    return
+    return greet
 
-greeting_1 = create_greeting('Good Morning', 'Luiz')
-greeting_2 = create_greeting('Good Night', 'Maycon')
+greeting_1 = create_greeting('Good Morning')
+greeting_2 = create_greeting('Good Night')
 
-print(greeting_1)
-print(greeting_2)
+print(greeting_1('Maycon'))
+print(greeting_2('Vitória'))
+
+# OR
+
+for names in lista:
+    print(greeting_2(names))
