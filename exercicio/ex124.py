9fb9588f8d5259ae0e834699afa6d08f178fc99a
Mayconalbuquerque <142945798+Mayconalbuquerque@users.noreply.github.com>
2023-11-14 16:41:41 -0300

    git commit -m

--- exercicio/ex124.py
+++ exercicio/ex124.py
@@ -2,6 +2,8 @@
 Exercício - sistema de perguntas e respostas
 '''
 
+
+
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
@@ -19,3 +21,34 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+
+
+# print(perguntas[0]['Pergunta'])
+# print(perguntas[0]['Opções'])
+# resposta = 'Digite a sua resposta: '
+# resposta_correta = 4
+
+# for opcao in perguntas[0]['Opções']:
+#     print(opcao)
+
+# print(resposta)
+
+# if resposta == resposta_correta:
+#     print('Você acertou!')
+
+# else:
+#     ('Você errou!')
+
+def pergunta_1(perguntas):
+    print(perguntas[0]['Pergunta'])
+    print(perguntas[0]['Opções'])
+    resposta = input('Digite a sua resposta: ')
+    resposta_correta = '4'
+    if resposta == resposta_correta:
+        print('Você acertou!')
+
+    else:
+        print('Você errou!')
+
+print(pergunta_1(perguntas)) 
+
