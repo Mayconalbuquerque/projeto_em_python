42eeefe5b546437334cf65587890b4c921b582e7
Mayconalbuquerque <mayconalbuquerque10@gmail.com>
2023-11-22 20:13:40 -0300

    git commit -m

--- intermediario_python/aula127.py
+++ intermediario_python/aula127.py
@@ -4,6 +4,17 @@ letras = set()
 
 while True:
     letra = input('Digite uma letra: ')
-    letras.add(letra)
+    numero_letras = len(letra)
+
+    if letra.isdigit():
+        print('Digite uma letra')
+        continue
+
+    elif numero_letras >= 2: 
+        print('Digite apenas uma letra')
+        continue
+
+    elif letra.isalpha():
+        letras.add(letra.lower())
 
     print(letras)
