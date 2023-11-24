5719de087d1f111aa8145dba093e7e80c428aafb
Mayconalbuquerque <mayconalbuquerque10@gmail.com>
2023-11-23 21:41:53 -0300

    git commit -m

---
+++ intermediario_python/aula132.py
@@ -0,0 +1,18 @@
+# Função Lambda em Python
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única expressão.
+
+lista = [
+    {'Nome': 'Maycon', 'Sobrenome': 'Albuquerque'},
+    {'Nome': 'Fatima', 'Sobrenome': 'Pereira'},
+    {'Nome': 'Rodrigo', 'Sobrenome': 'Morais'},
+    {'Nome': 'Rafael', 'Sobrenome': 'Morais'},
+    {'Nome': 'Amaro', 'Sobrenome': 'Albuquerque'},
+    {'Nome': 'Veronica', 'Sobrenome': 'Albuquerque'},
+]
+
+lista = [
+    4,23,1,32,4,5,6,6,
+]
