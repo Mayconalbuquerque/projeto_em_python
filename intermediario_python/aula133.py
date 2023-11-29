09c14afea0d50250f669af3a973ee6d5ebb5066d
Mayconalbuquerque <142945798+Mayconalbuquerque@users.noreply.github.com>
2023-11-29 16:31:15 -0300

    git commit -m

--- intermediario_python/aula133.py
+++ intermediario_python/aula133.py
@@ -4,6 +4,16 @@
 # que contém apenas uma linha. Ou seja, tudo
 # deve ser contido dentro de uma única expressão.
 
+
+# lista = [
+#     4, 23, 1, 32, 4, 5, 6, 6, 
+# ]
+
+# lista.sort(key=ordenar)
+# lista.sort(reverse = True)
+# sorted(lista)
+
+
 lista = [
     {'Nome': 'Maycon', 'Sobrenome': 'Albuquerque'},
     {'Nome': 'Fatima', 'Sobrenome': 'Pereira'},
@@ -17,11 +27,30 @@ lista = [
 #     4, 23, 1, 32, 4, 5, 6, 6, 
 # ]
 
-def ordenar(item):
-    return item['Nome']
+# def ordenar(item):
+#     return item['Nome']
 
-lista.sort(key=ordenar)
-# print(lista)
+# lista.sort(key=ordenar)
+# lista.sort(reverse = True)
+# sorted(lista)
 
-for item in lista:
-    print(item)
+# for item in lista:
+#     print(item)
+
+
+# or 
+
+# lista.sort(key=lambda item: item['Nome'])
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+l1 = sorted(lista, key = lambda item: item['Nome'])
+l2 = sorted(lista, key = lambda item: item['Sobrenome'])
+
+exibir(l1)
+exibir(l2)
+
+#CÓPIA RASA
